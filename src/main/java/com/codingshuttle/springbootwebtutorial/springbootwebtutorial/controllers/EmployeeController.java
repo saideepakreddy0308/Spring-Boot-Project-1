583d1a7b608fd2b832156a2d1fbd55b9fd064b75
MOON KNIGHT <90776053+saideepakreddy0308@users.noreply.github.com>
2024-10-09 00:04:03 +0530

    input validation annotations

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.Employe
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.services.EmployeeService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -21,10 +22,6 @@ import java.util.Optional;
 @RequestMapping(path = "/employees")
 public class EmployeeController {
 
-//    @GetMapping(path = "/")
-//    public String getMySuperSecretMessage(){
-//        return "Secret Message: Hello Deepak";
-//    }
 
     private final EmployeeService employeeService;
 
@@ -35,55 +32,40 @@ public class EmployeeController {
 
     @GetMapping("/{employeeId}")
     public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable(name = "employeeId") Long id) {
-//        return new EmployeeDTO(id, "Deepak", "deepak@gmail.com", 23, LocalDate.of(2024, 8, 3), true);
-//        return employeeRepository.findById(id).orElse(null);
         Optional<EmployeeDTO> employeeDTO =  employeeService.getEmployeeById(id);
-        // if employeeDTO is null, as said in employeeService
-        // we have two methods  notFound and ok
-//        if (employeeDTO == null) return ResponseEntity.notFound().build();
-//        return ResponseEntity.ok(employeeDTO);
         return employeeDTO
                 .map(employeeDTO1 -> ResponseEntity.ok(employeeDTO1))
                 .orElse(ResponseEntity.notFound().build());
-    }  // we get correct status code 404, if not found, rather than 500
+    }
 
-    //    http://localhost:8080/employees?age=23
     @GetMapping
     public ResponseEntity<List<EmployeeDTO>> getAllEmployees(@RequestParam(required = false, name = "age") Integer inputAge, @RequestParam(required = false) String sortBy) {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
-    // we need to make sure in controller that we are not using EmployeeEntity
 
 
     @PostMapping
-    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody EmployeeDTO inputEmployee) {
-//        return employeeService.createNewEmployee(inputEmployee);
-        // here we need to send some response that it is created successfully
+    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody @Valid EmployeeDTO inputEmployee) {
         EmployeeDTO savedEmployee = employeeService.createNewEmployee(inputEmployee);
-        // here we need to use new keyword, as we need to pass the ResponseEntity
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
-    // all  the above three methods are confirming to the MVC architecture
 
     @PutMapping(path = "/{employeeId}")
     public ResponseEntity<EmployeeDTO> updateEmployeeByID(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long employeeId) {
-        return ResponseEntity.ok(employeeService.updateEmployeeById(employeeId, employeeDTO));  // here, passing the employeeDTO to particular employeeId
+        return ResponseEntity.ok(employeeService.updateEmployeeById(employeeId, employeeDTO));
     }
 
     @DeleteMapping(path = "/{employeeId}")
-    public ResponseEntity<Boolean> deleteEmployeeByID(@PathVariable Long employeeId) {   // no need to return anything, void or boolean is best
-//        return ResponseEntity.ok(employeeService.deleteEmployeeById(employeeId));  // no need of returning anything
+    public ResponseEntity<Boolean> deleteEmployeeByID(@PathVariable Long employeeId) {
         boolean gotDeleted = employeeService.deleteEmployeeById(employeeId);
         if(gotDeleted) return ResponseEntity.ok(true);
         return ResponseEntity.notFound().build();
     }
 
-    @PatchMapping(path = "/{employeeId}")// we cant go for employeeDTO as input, as we really dont know the fields to update.
-    // so we instead use a map of string to object
+    @PatchMapping(path = "/{employeeId}")
     public ResponseEntity<EmployeeDTO> updatePartialEmployeeByID (@RequestBody Map<String, Object> updates, @PathVariable Long employeeId){
-        // here, in the method updatePartial in Service, we are returning null if does not exist.
         EmployeeDTO employeeDTO =  employeeService.updatePartialEmployeeById(employeeId, updates);  // here, passing the updates
         if ( employeeDTO == null) return  ResponseEntity.notFound().build();
         return ResponseEntity.ok(employeeDTO);
--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDTO.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDTO.java
@@ -1,10 +1,8 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -15,18 +13,36 @@ import java.time.LocalDate;
 public class EmployeeDTO {
 
     private Long id;
-
+    @NotBlank(message = "Name of the employee cannot be blank")
+    @Size(min=3, max = 10, message = "Number of characters in name should be in the range: [3,10]")
     private String name;
 
+    @Email(message = "Email should be a valid email")
     private String email;
 
+    @Max(value = 80, message = "Age cannot be greater than 80")
+    @Min(value = 10, message = "Salary of Employee cannot be less than 10")
     private Integer age;
 
+    @NotBlank(message = "Role of the employee cannot be blank")
+    @Pattern(regexp = "^(ADMIN|USER)$", message = "Role of Employee can be either USER or ADMIN")
+    private Integer role; // ADMIN, USER
+    // If some more complex, then we need to have custom annotation
+
+    @NotNull(message = "Salary of Employee should be not null")
+    @Positive(message = "Salary of Employee should be positive")
+    @Digits(integer = 6, fraction = 2, message = "The salary can be in the form XXXXXX.YY")
+    @DecimalMax(value = "100000.99")
+    @DecimalMin(value = "100.50")
+    private Double salary;
+
+    @PastOrPresent(message = "DateOfJoining field in Employee cannot be in the future")
     private LocalDate dateOfJoining;
 
-    // can have different name too
+    @AssertTrue(message = "Employee should be active")
     @JsonProperty("isActive")
     private Boolean isActive;
 
-    // getters and setters get confused with serialization, as JsonProperty is being used [ active, Isactive ], we define using lombok only
+    // custom annotations if like we need to have input as prime number
+    // Department is added here, then the @Valid goes to the department too, and gets its validation before finalizing input
 }
