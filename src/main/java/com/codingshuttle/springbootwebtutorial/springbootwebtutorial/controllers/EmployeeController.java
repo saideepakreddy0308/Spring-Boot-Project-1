04f18a99e50a1930195b9877029ee264e8471b97
MOON KNIGHT <90776053+saideepakreddy0308@users.noreply.github.com>
2024-10-08 15:47:45 +0530

    setting up service layer

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.controller
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.services.EmployeeService;
 import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,23 +19,26 @@ public class EmployeeController {
 //        return "Secret Message: Hello Deepak";
 //    }
 
-    private final EmployeeRepository employeeRepository;
+    private final EmployeeService employeeService;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
+
 
     @GetMapping("/{employeeId}")
     public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
 //        return new EmployeeDTO(id, "Deepak", "deepak@gmail.com", 23, LocalDate.of(2024, 8, 3), true);
-        return employeeRepository.findById(id).orElse(null);
+//        return employeeRepository.findById(id).orElse(null);
+        return employeeService.getEmployeeById(id);
     }
 
     //    http://localhost:8080/employees?age=23
     @GetMapping
     public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false, name = "age") Integer inputAge, @RequestParam(required = false) String sortBy) {
 //        return "Hi age " + inputAge + "  " + sortBy;
-        return employeeRepository.findAll();
+//        return employeeRepository.findAll();
+        return employeeService.getAllEmployees();
     }
 
     @PostMapping
@@ -43,7 +47,8 @@ public class EmployeeController {
 //        return null;   // Here not dealing with database
 //        inputEmployee.setId(100L);
 //        return inputEmployee;
-        return employeeRepository.save(inputEmployee);
+//        return employeeRepository.save(inputEmployee);
+        return employeeService.createNewEmployee(inputEmployee);
     }
 
     @PutMapping String updateEmployeeByID(){
---
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/services/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.services;
+
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public EmployeeEntity getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public List<EmployeeEntity> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public EmployeeEntity createNewEmployee(EmployeeEntity inputEmployee) {
+        return employeeRepository.save(inputEmployee);
+    }
+}
