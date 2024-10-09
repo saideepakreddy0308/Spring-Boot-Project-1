a796be6ac86b79771bdf326e47a1b30130cada5b
MOON KNIGHT <90776053+saideepakreddy0308@users.noreply.github.com>
2024-10-09 13:35:31 +0530

    transforming API response 2

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
@@ -18,7 +18,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     // This method handles the case where a requested resource (like an employee) is not found.
-    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception) {
+    // now instead of ApiError, we give ApiResponse, where it internally contains the ApiError in it.
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception) {
 
         // Build an ApiError object with the status of 404 (Not Found) and a message explaining the error
         ApiError apiError = ApiError.builder()
@@ -27,22 +28,22 @@ public class GlobalExceptionHandler {
                 .build();  // Build the final ApiError object
 
         // Return a ResponseEntity with the ApiError object and the HTTP status code of 404
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        return buildErrorResponseEntity(apiError);
     }
 
     @ExceptionHandler(Exception.class) // parent exception class
-    public ResponseEntity<ApiError> handleInternalServerError(Exception exception){
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception){
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .message(exception.getMessage())
                 .build();
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildErrorResponseEntity(apiError);
     }
 
     // we need clean messages so the reader easily understands the error, so define individually
     @ExceptionHandler(MethodArgumentNotValidException.class)
     // Here it combines all the exceptions we got, and bind it together
-    public ResponseEntity<ApiError> handleInputValidationError(MethodArgumentNotValidException exception){
+    public ResponseEntity<ApiResponse<?>> handleInputValidationError(MethodArgumentNotValidException exception){
         List<String> errors = exception
                     .getBindingResult()
                     .getAllErrors()
@@ -56,9 +57,16 @@ public class GlobalExceptionHandler {
                 .message(("Input Validation failed"))
                 .subErrors(errors)
                 .build();
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        // instead here we use the method
+        return buildErrorResponseEntity(apiError);
         // now we get concise errors as defined in the EmployeeDTO class or EmployeeRoleValidation , all of @Valid
         // also can change format of ApiError
     }
+
+    private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError) {
+        return  new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
+    }
 }
+
+// now the entities are returning ApiResponse
 
